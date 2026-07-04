72ff59f79e6464aa85aad509825d5e0cc1ea747a
Francis <flvthirty@gmail.com>
2026-07-04 16:22:47 +0800

    improve index scss import path

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '@/styles/styles.scss'
 import App from './App.jsx'
-import './styles/index.css'
+import '../css/index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
